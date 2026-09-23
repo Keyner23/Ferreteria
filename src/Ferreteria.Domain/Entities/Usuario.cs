b271b7ec9db92d06328ae99bc3d1b4fcff8bd9ab
Keyner Barrios <keinerba.ochoa@gmail.com>
2026-09-23 11:43:40 -0500

    Creacion de entidades

---
+++ src/Ferreteria.Domain/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferreteria.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Correo { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public Rol Rol { get; set; }
+        public bool Activo { get; set; } = true;
+        public DateTime FechaRegistro { get; set; }
+
+        public Cliente? Cliente { get; set; }
+    }
+}
